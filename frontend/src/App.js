c2f6e93dafc3dfd05da0612e16e17166aabaf0ae
ViniPolaca <vipolachini@uol.com.br>
2021-09-06 17:44:29 -0300

    altera app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,9 @@
 import React from 'react'
 import 'antd/dist/antd.css';
 
-import Perfil from './pages/PainelCPP/Parecer/index'
-
 function App() {
   return (
     <div className="App">
-      <Perfil></Perfil>
     </div>
   );
 }
