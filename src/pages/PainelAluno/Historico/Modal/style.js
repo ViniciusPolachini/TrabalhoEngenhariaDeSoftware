9f06509a3d1bfc8c6f239f377477a5e636d739e8
ViniPolaca <vipolachini@uol.com.br>
2021-09-20 20:36:23 -0300

    correção

---
+++ src/pages/PainelAluno/Historico/Modal/style.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components';
+
+export const Container = styled.div `
+    > div {
+        border: 1px lightGray solid;
+        padding: 5px 5px 5px 5px
+    }
+    > label {
+        display: block;
+        position: relative;
+        margin-top: 2vh;
+    }
+`;
